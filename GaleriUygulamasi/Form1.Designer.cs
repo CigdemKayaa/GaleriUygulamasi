dea7d28a3a10d9097c7fdc5f97934b55984906ac
CigdemKayaa <49146485+CigdemKayaa@users.noreply.github.com>
2019-04-03 11:16:49 +0300

    Initial commit

---
+++ GaleriUygulamasi/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace GaleriUygulamasi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.MenuDosya = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuAc = new System.Windows.Forms.ToolStripMenuItem();
+            this.MenuKaydet = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem1 = new System.Windows.Forms.ToolStripSeparator();
+            this.MenuEkle = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripSeparator();
+            this.MenuCikis = new System.Windows.Forms.ToolStripMenuItem();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuDosya});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // MenuDosya
+            // 
+            this.MenuDosya.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MenuAc,
+            this.MenuKaydet,
+            this.toolStripMenuItem1,
+            this.MenuEkle,
+            this.toolStripMenuItem2,
+            this.MenuCikis});
+            this.MenuDosya.Name = "MenuDosya";
+            this.MenuDosya.Size = new System.Drawing.Size(51, 20);
+            this.MenuDosya.Text = "Dosya";
+            // 
+            // MenuAc
+            // 
+            this.MenuAc.Name = "MenuAc";
+            this.MenuAc.Size = new System.Drawing.Size(180, 22);
+            this.MenuAc.Text = "Aç";
+            this.MenuAc.Click += new System.EventHandler(this.MenuAc_Click);
+            // 
+            // MenuKaydet
+            // 
+            this.MenuKaydet.Name = "MenuKaydet";
+            this.MenuKaydet.Size = new System.Drawing.Size(180, 22);
+            this.MenuKaydet.Text = "Kaydet";
+            this.MenuKaydet.Click += new System.EventHandler(this.MenuKaydet_Click);
+            // 
+            // toolStripMenuItem1
+            // 
+            this.toolStripMenuItem1.Name = "toolStripMenuItem1";
+            this.toolStripMenuItem1.Size = new System.Drawing.Size(177, 6);
+            // 
+            // MenuEkle
+            // 
+            this.MenuEkle.Name = "MenuEkle";
+            this.MenuEkle.Size = new System.Drawing.Size(180, 22);
+            this.MenuEkle.Text = "Ekle";
+            this.MenuEkle.Click += new System.EventHandler(this.MenuEkle_Click);
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(177, 6);
+            // 
+            // MenuCikis
+            // 
+            this.MenuCikis.Name = "MenuCikis";
+            this.MenuCikis.Size = new System.Drawing.Size(180, 22);
+            this.MenuCikis.Text = "Çıkış";
+            this.MenuCikis.Click += new System.EventHandler(this.MenuCikis_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(800, 426);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem MenuDosya;
+        private System.Windows.Forms.ToolStripMenuItem MenuAc;
+        private System.Windows.Forms.ToolStripMenuItem MenuKaydet;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem1;
+        private System.Windows.Forms.ToolStripMenuItem MenuEkle;
+        private System.Windows.Forms.ToolStripSeparator toolStripMenuItem2;
+        private System.Windows.Forms.ToolStripMenuItem MenuCikis;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ GaleriUygulamasi/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GaleriUygulamasi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuEkle_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "JPEG Resimler (*.jpg;*.jpeg)|*.jpg;*.jpeg|PNG Resimler(*.png)|*.png|Tüm Dosyalar(*.jpg;*.jpeg;*.png;*.bmp)|*.jpg;*.jpeg;*.png;*.bmp";
+            ofd.FilterIndex=3;
+            ofd.Title = "Resimleri Seç";
+            ofd.Multiselect = true;
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            if (ofd.ShowDialog()==DialogResult.OK)
+            {
+                string[] dosyaKonumlari = ofd.FileNames;
+                foreach (var dosyaKonum in dosyaKonumlari)
+                {
+                    PictureBoxEkle(dosyaKonum);
+                }
+            }
+
+        }
+
+        private void PictureBoxEkle(string dosyaKonum) 
+        {
+            Panel pnl = new Panel();
+            pnl.BorderStyle = BorderStyle.FixedSingle;
+            pnl.Width = 75;
+            pnl.Height = 75;
+
+            PictureBox pb = new PictureBox();
+
+            pb.SizeMode = PictureBoxSizeMode.Zoom;
+            pb.ImageLocation = dosyaKonum;
+            pb.Dock = DockStyle.Fill;
+
+            pb.Click += Pb_Click;//pb her tıklanıdıgında bıze verı versın dıye yapıldı.
+
+            pnl.Controls.Add(pb);
+            flowLayoutPanel1.Controls.Add(pnl);
+            
+        }
+
+        private void Pb_Click(object sender, EventArgs e)
+        {
+            PictureBox pbBox =  (PictureBox)sender;
+
+            Form frm = new Form();
+            frm.Text = "Resim Önizleme";
+            frm.Width = 150;
+            frm.Height = 150;
+
+            PictureBox pbNew = new PictureBox();
+            pbNew.ImageLocation = pbBox.ImageLocation;
+            pbNew.SizeMode = pbBox.SizeMode;
+            pbNew.Dock = DockStyle.Fill;
+            frm.Controls.Add(pbNew);
+            frm.Show();
+        }
+
+        private void MenuKaydet_Click(object sender, EventArgs e)
+        {
+            List<string> kaydedilecekdosyaKonumlari = new List<string>();
+            foreach (Panel pnl in flowLayoutPanel1.Controls)
+            {
+                PictureBox pbox = (PictureBox)pnl.Controls[0];
+                kaydedilecekdosyaKonumlari.Add(pbox.ImageLocation);
+            }
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Title = "Galeri Kaydet";
+            sfd.Filter = "Galeri Dosyaları(*.glr)|*.glr";
+            sfd.FilterIndex = 1;
+            sfd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            if (sfd.ShowDialog()==DialogResult.OK)
+            {
+                System.IO.File.WriteAllLines(sfd.FileName, kaydedilecekdosyaKonumlari);
+                MessageBox.Show("Galeriye kaydedilmiştir. ");
+            }
+
+
+        }
+
+        private void MenuAc_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Galeri Kaydet";
+            ofd.Filter = "Galeri Dosyaları(*.glr)|*.glr";
+            ofd.FilterIndex = 1;
+            ofd.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+            if (ofd.ShowDialog()==DialogResult.OK)
+            {
+                string[] dosyalar = System.IO.File.ReadAllLines(ofd.FileName);
+                foreach (string dosyaKonum in dosyalar)
+                {
+                    PictureBoxEkle(dosyaKonum);
+                }
+            }
+        }
+
+        private void MenuCikis_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
